a513d6b070f094bc3cfbc0a200485277fd143e24
OlegS19 <olegsergeevich19@yandex.ru>
2023-09-08 16:57:24 +0300

    Diplom #2 fix: устранены замечания

--- stats-service/server/src/main/java/ru/practicum/model/StatHit.java
+++ stats-service/server/src/main/java/ru/practicum/model/StatHit.java
@@ -16,16 +16,14 @@ public class StatHit {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private int id;
 
-    @Column(name = "app")
     private String app;
 
-    @Column(name = "uri")
+
     private String uri;
 
-    @Column(name = "ip")
+
     private String ip;
 
     @Column(name = "created")
